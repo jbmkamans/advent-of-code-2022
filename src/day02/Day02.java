2f2e931278aa29c23e1f9e0942d835743b62c7cb
Jim Kamans <j.b.m.kamans@hva.nl>
2022-12-02 14:44:03 +0100

    Solve day02 part 2

--- src/day02/Day02.java
+++ src/day02/Day02.java
@@ -11,21 +11,26 @@ public class Day02 {
         Scanner input = new Scanner(file);
 
         int totalPoints = 0;
+        long totalPointsPart2 = 0;
         while (input.hasNext()) {
             String line = input.nextLine();
             String[] round = line.split(" ");
             totalPoints += getPoints(round[0].charAt(0), round[1].charAt(0));
+            totalPointsPart2 += getPointsPart2(round[0].charAt(0), round[1].charAt(0));
         }
-        
+
         // Part 1: how many points do you get following the strategy guide
         System.out.println(totalPoints);
+
+        // Part 2: how many points do you get following the strategy guide
+        System.out.println(totalPointsPart2);
     }
 
     public static int getPoints(char encryptedMoveOpponent, char encryptedMoveYou) {
         final int DRAW = 3;
         final int WIN = 6;
 
-        /**
+        /*
          * Convert encrypted moves to specific numbers, the number of points you get for using a certain move:
          *  A == X == rock == 1
          *  B == Y == paper == 2
@@ -34,7 +39,7 @@ public class Day02 {
         int moveOpponent = encryptedMoveOpponent - 'A' + 1;
         int moveYou = encryptedMoveYou - 'X' + 1;
 
-        /**
+        /*
          * Add points for a WIN or DRAW (losing gives you no extra points).
          * Use "moveYou" as a "total points this round"-variable, because why not?
          * If both players have the same move, it's a DRAW.
@@ -48,5 +53,35 @@ public class Day02 {
         }
 
         return moveYou;
+    }
+
+    public static int getPointsPart2(char encryptedMoveOpponent, char encryptedOutcome) {
+        final int DRAW = 3;
+        final int WIN = 6;
+
+        /*
+         * Convert encrypted moves and outcomes to specific numbers, the number of points each is worth:
+         *  A = rock = 1, B = paper = 2, C = scissors = 3
+         *  X = LOSE = 0, Y = DRAW = 3, Z = WIN = 6
+         */
+        int moveOpponent = encryptedMoveOpponent - 'A' + 1;
+        int outcome = (encryptedOutcome - 'X') * 3;
+        int yourPoints = outcome;
+
+        /*
+         * Add points for using the correct move to get the outcome.
+         * If outcome is a DRAW, use the same move.
+         * To get the move for WIN or LOSE, use some maths so I don't need to write out multiple conditionals for each
+         * move combination.
+         */
+        if (outcome == DRAW) {
+            yourPoints += moveOpponent;
+        } else if (outcome == WIN) {
+            yourPoints += moveOpponent % 3 + 1;
+        } else {
+            yourPoints += (moveOpponent + 1) % 3 + 1;
+        }
+
+        return yourPoints;
     }
 }
